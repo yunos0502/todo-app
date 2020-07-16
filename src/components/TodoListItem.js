95373daa7b8e97838f09dffa1cfd103d53dcc255
Park Gyu Ae <yunos0502@gmail.com>
2020-07-16 16:35:11 +0900

    11장 컴포넌트 성능 최적화 update

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,23 +7,28 @@ import {
 import classnames from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
-      <div
-        className={classnames("checkbox", { checked })}
-        onClick={() => onToggle(id)}
-      >
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={classnames("checkbox", { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps === nextProps
+);
